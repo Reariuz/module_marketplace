5eb8b8378e0eaf78d5a284219c388ae2a6cf9949
Reariuz <118771123+Reariuz@users.noreply.github.com>
2024-10-25 20:50:27 +0000

    first implementation of i18n translation. now i can use multiple languages and ask other for support

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import i18n from 'i18next'; 
+
 
 const Sidebar = () => {
+
+    const { t } = useTranslation();
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+      };
+
     return (
         <aside className="sidebar">
-            <h2>Categories</h2>
+            <h2>{t('welcome')}</h2>
             <ul>
                 <li>Action</li>
                 <li>Adventure</li>
                 <li>RPG</li>
                 <li>Shooter</li>
                 <li>Indie</li>
+                <li><button onClick={() => changeLanguage('de')}>Deutsch</button></li>
+                <li><button onClick={() => changeLanguage('en')}>English</button></li>
             </ul>
         </aside>
     );
